b6b5719542c1421ef74df6de80a14e93c6fa7e7d
Carlos Antunes <carlos.rafael@gec.inatel.br>
2017-06-29 22:37:44 -0300

    adição de comentários e indentação

--- public/javascripts/controller/controller.js
+++ public/javascripts/controller/controller.js
@@ -1,17 +1,20 @@
 (function(){
-
+    //criação do módulo de nome demo, que possui um controller e um serviço
     angular.module('demo', [])
     .controller('myController', myController)
     .service('MyService', MyService);
 
+    //invoca todas as dependências do controller
     myController.inject=['$scope','$location', '$http','MyService', '$window'];
+    //função controller
     function myController($scope, $location, $http,MyService, $window){
-        
- $scope.popitup = function(url, target) {
-  $window.open(url, target);
-}
 
-        
+        //função que abrirá nova janela        
+        $scope.popitup = function(url, target) {
+            $window.open(url, target);
+        }
+
+        //modelo de um objeto instrument    
         $scope.instrument = {
             id:'',
             name:'',
@@ -19,31 +22,32 @@
             detail:''
         };
 
+        //sempre que o usuário atualizar a página, a função que retorna todos os instrumentos será invocada
         $scope.instruments= MyService.getinstruments($http, $scope);
 
+        //função auxiliar que escreve os valores de um objeto aos input fields do form
         $scope.writeinstrument = function(index){
-            console.log('aaa',index);
+
             $scope.instrument.id= $scope.instruments[index].id;
             $scope.instrument.name= $scope.instruments[index].name;
             $scope.instrument.price= $scope.instruments[index].price;
             $scope.instrument.detail= $scope.instruments[index].detail;
 
-//            MyService.editinstrument($http,$scope,$scope.instrument);
         };
 
-
+        //função da controller que adiciona um novo instrumento
         $scope.addinstrument = function(){
+            //a adição é realizada através de um serviço
             MyService.addinstrument($http,$scope, $scope.instrument.id, $scope.instrument.name, $scope.instrument.price, $scope.instrument.detail);
-            console.log('chamou a addinstrument()');
         };
 
+        //função qeu deleta um instrumento
         $scope.deleteinstrument = function(index){
-            console.log('agora eh ', index);
+            //a operação de remoção é relaizada por um serviço
             MyService.deleteinstrument($http, $scope, index)
         };
 
-  
-
+        //função que atualiza um instrumento
         $scope.editinstrument = function(instrumentId, instrumentName, instrumentPrice, instrumentDetail){
 
             $scope.instrument.id= instrumentId;
@@ -51,16 +55,17 @@
             $scope.instrument.price= instrumentPrice;
             $scope.instrument.detail= instrumentDetail;
             console.log($scope.instrument);
+            //a atualização ocorre através de um serviço
             MyService.editinstrument($http,$scope,$scope.instrument);
         };
-
-
     }
 
+    //criação do serviço
     function MyService(){
         var service = this;
         var instruments=[];
 
+        //serviço invocado pela controller que adiciona um novo instrumento ao banco
         service.addinstrument = function($http,$scope,Id,Name, Price, Detail){
             console.log('id1 eh ',Id);
             data={
@@ -69,77 +74,80 @@
                 price:Price,
                 detail:Detail
             };
-            console.log(JSON.stringify(data));
+            //console.log(JSON.stringify(data));
+            
+            //chama uma operação post com a rota necessária, e o objeto em json
             $http.post('http://localhost:3000/Instruments/', JSON.stringify(data)).then(function (response) {
                 if (response.data)
                     $scope.msg = "Post Data Submitted Successfully!";
-                    console.log(response.data);
-                    console.log($scope);
-//                    $scope.instruments.push(data);
-                     $scope.instruments= service.getinstruments($http, $scope);
-                     
+                    //console.log(response.data);
+                    //console.log($scope);
+                    //$scope.instruments.push(data);
 
-                    //$scope.$digest();
+                    //novo get é invocado para atualizar a view
+                     $scope.instruments= service.getinstruments($http, $scope);
+    
                 }, function (response) {
+                    //em caso de erro
                     $scope.msg = "Service not Exists";
                     $scope.statusval = response.status;
                     $scope.statustext = response.statusText;
                     $scope.headers = response.headers();
-                    console.log($scope);
-//                    window.location.href = '/instruments'
-                    
+                    //console.log($scope);                    
                 });
                 
-            
+            //o array instruments recebe um novo elemento
             instruments.push(data);
         };
 
+        //serviço que retorna todos os instrumentos do banco
         service.getinstruments = function($http, $scope){
-
+            //operação get
             $http.get('http://localhost:3000/Instruments').
             then(function(response){
-                console.log('realizou getAll',response.data);
+                //console.log('realizou getAll',response.data);
                 $scope.instruments = response.data;
                 
             });
-
+            //serviço retorna todos os instrumentos presentes no banco
             return instruments;
         }
 
-        
-
+        //serviço que deleta um instrumento
         service.deleteinstrument = function($http, $scope, index){
-            // Simple Delete request example:
+
             index = index.id;
-            console.log('ssasas'+index);
+
             var url = 'http://localhost:3000/Instruments/'+index, data = 'parameters';
+            //operação delete que deleta um instrumento do banco
             $http.delete(url, data).then(function (response) {
-                console.log('sucesso');
-                $scope.instruments= service.getinstruments($http, $scope);
-            // This function handles success
+            //console.log('sucesso');
+            $scope.instruments= service.getinstruments($http, $scope);
+            
         }, function (response) {
             console.log('erro');
-            // this function handles error
+            
             });
         }
 
+        //serviço que edita um instrumento
         service.editinstrument = function($http,$scope,instrument){
-            console.log('id1 eh ',instrument);
-            data={
+             //cria modelo com informações do objeto selecionado pelo usuário para edição
+             data={
                 id:instrument.id,
                 name:instrument.name,
                 price:instrument.price,
                 detail:instrument.detail
             };
-            console.log(JSON.stringify(data));
+            //console.log(JSON.stringify(data));
 
+            //operação put que passa o id do instrumento como parâmetro, e o objeto em json no corpo da requisição
             $http.put('http://localhost:3000/Instruments/'+data.id, JSON.stringify(data)).then(function (response) {
                 if (response.data)
                     $scope.msg = "Put Data Submitted Successfully!";
-                    console.log(response.data);
-                    console.log($scope);
-//                    $scope.instruments.push(data);
-                     $scope.instruments= service.getinstruments($http, $scope);
+                    //console.log(response.data);
+                    //console.log($scope);
+                    $scope.instruments= service.getinstruments($http, $scope);
                      
                     //$scope.$digest();
                 }, function (response) {
@@ -147,15 +155,10 @@
                     $scope.statusval = response.status;
                     $scope.statustext = response.statusText;
                     $scope.headers = response.headers();
-                    console.log($scope);
-//                    window.location.href = '/instruments'
+                    //console.log($scope);
                     
-                });
-                
-            
-        //    instruments.push(data);
+                });                
         };
-
     }
 
 
--- public/javascripts/controller/reportController.js
+++ public/javascripts/controller/reportController.js
@@ -1,30 +1,32 @@
 (function(){
-
+    //criação do módulo de nome demo, controller e serviço
     angular.module('demo', [])
     .controller('myController', myController)
     .service('MyService', MyService);
 
+    //inserção das dependências
     myController.inject=['$scope','$location', '$http','MyService', '$window'];
+    //função controller
     function myController($scope, $location, $http,MyService, $window){
-  
+        
+        //controller chama serviço que invoca todos os instrumentos
         $scope.soma= MyService.getinstruments($http, $scope);
         console.log("aifjaoifejaoeifjaoeif", $scope.soma);
         $scope.writeinstrument = function(index){
-            console.log('aaa',index);
             $scope.instrument.id= $scope.instruments[index].id;
             $scope.instrument.name= $scope.instruments[index].name;
             $scope.instrument.price= $scope.instruments[index].price;
             $scope.instrument.detail= $scope.instruments[index].detail;
-
-//            MyService.editinstrument($http,$scope,$scope.instrument);
         };
    }
-
+    //função serviço
     function MyService(){
         var service = this;
         var instruments=[];
 
+        //serviço que retorna todos os intrumentos
         service.getinstruments = function($http, $scope){
+            //variáveis de controle
             $scope.soma = 0;
             $scope.count = 0;
             $scope.nomeMaiorPreco = '';
@@ -34,11 +36,12 @@
             var quant=0;
             var nomeMaiorP = '';
             var nomeMenorP = '';
+
+            //operação GET
             $http.get('http://localhost:3000/Instruments').
             then(function(response){
-                console.log('dsdsdsd',response.data.length);
-                console.log('realizou getAll',response.data);
 
+                //varre todos os instrumentos, e lista o valor total de todos os instrumentos, o nome do mais e menos valioso, com seus respectivos preços
                 for(var i=0;i< response.data.length;i++){
                     
                     quant = parseInt(response.data[i].price);
@@ -55,25 +58,23 @@
 
                     $scope.soma += quant;
                     console.log('soma eh ', $scope.soma);
-
-
                 }
+
                 $scope.maior = maior;
                 $scope.menor = menor;
-                console.log($scope.maior);
+                //console.log($scope.maior);
                 $scope.count=response.data.length;
-                console.log('count eh ',$scope.count);
+                //console.log('count eh ',$scope.count);
 
                 $scope.nomeMaiorPreco = nomeMaiorP;
                 $scope.nomeMenorPreco = nomeMenorP;
-                console.log($scope.nomeMaiorPreco); 
-                console.log($scope.nomeMenorPreco);
+                //console.log($scope.nomeMaiorPreco); 
+                //console.log($scope.nomeMenorPreco);
                 return $scope.soma;
             });
 
             return $scope.soma;
         }
-
     }
 
 
