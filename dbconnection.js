2c7286513ad96dfb66b0dd3d808f39c7eda117a9
Carlos Antunes <carlos.rafael@gec.inatel.br>
2017-06-30 19:20:58 -0300

    Adição dos parâmetros para o banco ClearDB

--- dbconnection.js
+++ dbconnection.js
@@ -3,10 +3,15 @@ var mysql = require('mysql');
 
 //cria a comunicação com o banco, definindo o host, usuário, senha e o nome do banco de dados
 var connection = mysql.createPool({
+    /*
     host:'localhost',
     user:'root',
     password:'root',
-    database:'carlos_dm104'
+    database:'carlos_dm104'*/
+    host:'us-cdbr-iron-east-03.cleardb.net',
+    user:'b01db4ba31c183',
+    password:'d3978514',
+    database:'heroku_50aed392026ddbe'
 });
 //torna a var connection acessível em outros módulos da aplicação
 module.exports=connection;
